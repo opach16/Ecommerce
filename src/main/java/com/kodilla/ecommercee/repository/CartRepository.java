d6990742749263b68c2e71e408772d38d88af465
Marcin Pająk <39952424+pelemistrz@users.noreply.github.com>
2024-11-12 14:06:29 +0100

    Remove unnecessary methods

--- src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
@@ -14,9 +14,4 @@ public interface CartRepository extends CrudRepository<Cart, Long> {
     @Override
     List<Cart> findAll();
 
-    Optional<Cart> findById(Long id);
-
-    Cart save(Cart order);
-
-    void deleteById(Long id);
 }
