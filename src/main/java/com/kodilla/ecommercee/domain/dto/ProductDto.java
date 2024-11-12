049a7644ac83f672bf2372324d34f5686170d971
Konrad Opach <opach16@outlook.com>
2024-11-12 19:06:29 +0100

    JDP241001-21 - Added real implementation for ProductController

--- src/main/java/com/kodilla/ecommercee/domain/dto/ProductDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/dto/ProductDto.java
@@ -1,12 +1,16 @@
 package com.kodilla.ecommercee.domain.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
+@Data
 @AllArgsConstructor
-@Getter
+@NoArgsConstructor
+@Builder
 public class ProductDto {
     private Long id;
     private Long groupId;
