32d3a5e61dea772ef1865bd9bc31932300fe024a
Konrad Opach <opach16@outlook.com>
2024-11-17 23:48:06 +0100

    Refactoring

--- src/main/java/com/kodilla/ecommercee/exception/OrderNotFoundException.java
+++ src/main/java/com/kodilla/ecommercee/exception/OrderNotFoundException.java
@@ -2,6 +2,6 @@ package com.kodilla.ecommercee.exception;
 
 public class OrderNotFoundException extends Exception{
     public OrderNotFoundException(){
-        super("Order with give Id has not been found");
+        super("Order with given ID has not been found");
     }
 }
