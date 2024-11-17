1cc08c5f18ce715df576fade523443d281943201
Konrad Opach <opach16@outlook.com>
2024-11-17 23:47:04 +0100

    Changed class inheritance to Exception

--- src/main/java/com/kodilla/ecommercee/exception/UserNotFoundException.java
+++ src/main/java/com/kodilla/ecommercee/exception/UserNotFoundException.java
@@ -1,7 +1,6 @@
 package com.kodilla.ecommercee.exception;
 
-public class UserNotFoundException extends RuntimeException {
-
+public class UserNotFoundException extends Exception {
     public static final String BY_ID_MESSAGE = "User with given ID has not been found";
     public static final String BY_USERNAME_MESSAGE = "User has not been found";
     public static final String IS_BLOCKED_MESSAGE = "User has already been blocked";
